f50b85e72c0d46125b74d8dd4f366c267c01da4f
evan <evan.ecater@gmail.com>
2016-02-06 20:24:22 -0500

    speech

---
+++ speech.py
@@ -0,0 +1,38 @@
+import time
+import threading
+
+class MyThread(threading.Thread):
+    def run(self):
+        '''Start your thread here'''
+        time.wait(1)
+        print 'hello'
+
+        pass
+
+thread = MyThread()
+thread.daemon = True
+thread.start()
+print 'test'
+
+
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+    print sr.Microphone
+    print r # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
